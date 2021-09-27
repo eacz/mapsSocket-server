a93577edf06f48b6616cdf2aa9bbcaea53eed798
Esteban Canteros <estebanacanteros@gmail.com>
2021-09-27 12:23:14 -0300

    marker & markers classes added

--- models/sockets.js
+++ models/sockets.js
@@ -1,12 +1,19 @@
+const Markers = require("./markers")
+
 class Sockets {
   constructor(io) {
     this.io = io
+    this.marker = new Markers()
     this.socketEvents()
   }
 
   socketEvents() {
     this.io.on('connection', (socket) => {
+        //TODO: active markers event
+
+        //TODO: new marker event
         
+        //TODO: update marker event
     })
   }
 }
